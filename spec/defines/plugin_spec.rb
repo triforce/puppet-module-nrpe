f94b3ed478d6049660b84a3c9fef1f6afe6a24b7
Chris Andrews <christophersimonandrews@gmail.com>
2018-01-22 12:00:22 +0000

    Changed the package_name parameter in the plugin resource definition to use the package_name from the nrpe class by default. Added tests to check this.

--- spec/defines/plugin_spec.rb
+++ spec/defines/plugin_spec.rb
@@ -2,20 +2,37 @@ require 'spec_helper'
 
 describe 'nrpe::plugin', :type => :define do
 
+  let :facts do
+    {
+      :osfamily     => 'Debian',
+      :architecture => 'x86_64',
+    }
+  end
+
+  let (:title) {'check_users'}
+  let :params do
+    {
+      :ensure  => 'present',
+    }
+  end
+
+  context 'default module params' do
     let(:pre_condition) { 'include nrpe' }
-
-    let :facts do
-     { :osfamily     => 'Debian', 
-       :architecture => 'x86_64',}
-    end
-
-    let (:title) {'check_users'}
-    let :params do
-      {
-        :ensure  => 'present',
-      }
-    end
 
     it { is_expected.to compile.with_all_deps }
     it { should contain_file('/usr/lib/nagios/plugins/check_users') }
+  end
+
+  context 'include nrpe with different package name' do
+    let(:pre_condition) do
+      '
+      class { "nrpe":
+        package_name => "nrpe",
+      }
+      '
+    end
+
+    it { is_expected.to compile.with_all_deps }
+    it { should contain_file('/usr/lib/nagios/plugins/check_users').with_require('Package[nrpe]') }
+  end
 end
